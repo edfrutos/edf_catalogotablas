cb7503038826e0606b1dbde3be67b51ae388ba26
edefrutos <16051333+edfrutos@users.noreply.github.com>
2025-06-03 20:10:33 +0200

    Creación de archivos __init__.py=============================== en subcarpetas de /tools

---
+++ tools/admin_utils/__init__.py
@@ -0,0 +1,7 @@
+# Script: __init__.py
+# Descripción: [Explica brevemente qué hace el script]
+# Uso: python3 __init__.py [opciones]
+# Requiere: [librerías externas, si aplica]
+# Variables de entorno: [si aplica]
+# Autor: [Tu nombre o equipo] - 2025-06-03
+
